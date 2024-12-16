fce288ad427dcce9ad74944dd733026fab9c641a
Maria Natalia Martin Dans <natalia.martin@enginyers.net>
2024-12-16 18:21:37 +0100

    test: more scenes

---
+++ scenes/M/show_espirales.sh
@@ -0,0 +1,3 @@
+../../miniRT espiral_front_full.rt &
+../../miniRT espiral_front.rt &
+../../miniRT espiral_top.rt &
--- scenes/camera/test_camera.sh
+++ scenes/camera/test_camera.sh
@@ -1,3 +1,6 @@
-../../miniRT one_plane_h.rt &
-../../miniRT plane_h_above_camera.rt &
+../../miniRT plane_h_front_above_camera.rt &
+../../miniRT plane_h_front_below_camera.rt &
+../../miniRT plane_v_right_camera.rt &
+../../miniRT plane_v_left_camera.rt &
+../../miniRT plane_h_top_camera.rt &
 ../../miniRT plane_h_below_camera.rt &
