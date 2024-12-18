abedcc95bbe7c8cddff8c148944a835d864590cb
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2024-12-18 10:03:16 +0100

    test: Escenas Evaluacion

---
+++ scenes/evaluacion/show_evaluation.sh
@@ -0,0 +1,6 @@
+../../miniRT semiesfera_bg_black_000A.rt &
+../../miniRT semiesfera_bg_white_000A.rt &
+../../miniRT semiesfera_bg_peach_000A.rt &
+../../miniRT semiesfera_bg_black_500A.rt &
+../../miniRT semiesfera_bg_white_500A.rt &
+../../miniRT semiesfera_bg_peach_500A.rt &
